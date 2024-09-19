9d7466f325e672eb1c968f5e22a9df2aa18dc951
Alireza Jalili <alirlikh@gmail.com>
2024-09-19 08:23:51 +0330

    feat: add task

--- src/Pages/AddTask.jsx
+++ src/Pages/AddTask.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from "react"
+import TaskBody from "../Component/Task-Body/TaskBody"
+import { Col, Row } from "react-bootstrap"
 
 const AddTask = () => {
   return (
-    <div>Add</div>
+    <Row className="p-4">
+      <Col sm={8} md={8} className="mx-auto">
+        <h2 className="text-cnter mt-2  mb-5 ">Create New Task</h2>
+        <TaskBody />
+      </Col>
+    </Row>
   )
 }
 
