a979ef5ee556f0dd3f7664d687214d28a019e39d
Alireza Jalili <alirlikh@gmail.com>
2024-09-19 20:58:53 +0330

    feat: add alert to serach query

--- src/Component/TaskList/TaskList.jsx
+++ src/Component/TaskList/TaskList.jsx
@@ -1,53 +1,25 @@
 import React, { useEffect } from "react"
 import TaskItem from "../TaskItem/TaskItem"
-import { Button, Col, Row } from "react-bootstrap"
+import { Alert, Button, Col, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { fetchTasks } from "../../Store/TaskSlice"
 
 const TaskList = ({ searchQuery }) => {
-  // const taskList = [
-  //   {
-  //     id: 1,
-  //     todo: "task1",
-  //     todo: "orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
-  //     completed: false
-  //   },
-  //   {
-  //     id: 2,
-  //     todo: "task",
-  //     todo: "orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
-  //     completed: false
-  //   },
-  //   {
-  //     id: 3,
-  //     todo: "task",
-  //     todo: "orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
-  //     completed: true
-  //   },
-  //   {
-  //     id: 4,
-  //     todo: "task",
-  //     todo: "orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
-  //     completed: false
-  //   },
-  //   {
-  //     id: 5,
-  //     todo: "task",
-  //     todo: "orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ",
-  //     completed: true
-  //   }
-  // ]
-
   const dispatch = useDispatch()
   const tasks = useSelector((state) => state.tasks.items)
   const taskStatus = useSelector((state) => state.tasks.status)
   const error = useSelector((state) => state.tasks.error)
 
+  const filtredTask = tasks.filter((task) =>
+    //   // task.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    //   // task.description.toLowerCase().includes(searchQuery.toLowerCase())
+    searchQuery ? task.todo.toLowerCase().includes(searchQuery.toLowerCase()) : task
+  )
+
   useEffect(() => {
     // if (taskStatus === "idle")
     dispatch(fetchTasks())
-    console.log("call fetch")
   }, [dispatch])
 
   if (taskStatus === "loading") return <div className="text-center pt-5 fw-1 fs-3 ">loading...</div>
@@ -56,19 +28,18 @@ const TaskList = ({ searchQuery }) => {
   return (
     <>
       <h2 className="title m-3 p-1 mt-5 ml-4">Today's Task</h2>
-      <Row className="flex-md-row ">
-        {tasks
-          ?.filter((task) =>
-            // task.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            // task.description.toLowerCase().includes(searchQuery.toLowerCase())
-
-            searchQuery ? task.todo.toLowerCase().includes(searchQuery.toLowerCase()) : task
-          )
-          .map((task) => (
+      <Row className="flex-md-row justify-content-center">
+        {filtredTask && filtredTask.length > 0 ? (
+          filtredTask.map((task) => (
             <Col key={task.id} xs={12} md={6}>
               <TaskItem task={task} />
             </Col>
-          ))}
+          ))
+        ) : (
+          <Alert variant="danger" className="mb-0">
+            <span>Oops! Your Task Not Found.</span>
+          </Alert>
+        )}
         <Link
           to={"/create"}
           className="btn my-5 mx-3 text-white"
