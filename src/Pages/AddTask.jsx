2a48f447bc5915bc99fc56b70208d8bc86644ed0
Alireza Jalili <alirlikh@gmail.com>
2024-09-20 11:41:37 +0330

    feat: change page style

--- src/Pages/AddTask.jsx
+++ src/Pages/AddTask.jsx
@@ -4,9 +4,13 @@ import { Col, Row } from "react-bootstrap"
 
 const AddTask = () => {
   return (
-    <Row className="p-4">
+    <Row className="p-4 justify-content-center">
+      <Row className="w-100">
+        <Col sm={12} md={10} className="mx-auto  ">
+          <h2 className="text-cnter mt-2  mb-5 title-line pb-3">Create New Task</h2>
+        </Col>
+      </Row>
       <Col sm={8} md={8} className="mx-auto">
-        <h2 className="text-cnter mt-2  mb-5 ">Create New Task</h2>
         <TaskBody />
       </Col>
     </Row>
--- src/Pages/EditeTask.jsx
+++ src/Pages/EditeTask.jsx
@@ -5,8 +5,12 @@ import { Col, Row } from "react-bootstrap"
 const EditeTask = () => {
   return (
     <Row className="p-4">
+      <Row className="w-100 justify-content-center">
+        <Col sm={12} md={10} className="mx-auto  ">
+          <h2 className="text-cnter mt-2  mb-5 title-line pb-3">Edite</h2>
+        </Col>
+      </Row>
       <Col sm={8} md={8} className="mx-auto">
-        <h2 className="text-cnter mt-2  mb-5 ">Edite</h2>
         <TaskBody />
       </Col>
     </Row>
